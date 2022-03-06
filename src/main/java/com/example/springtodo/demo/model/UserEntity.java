494ffb7d9050d0c1c72854e0fa6b8da952ec2c2d
sagada <wlatmdqja@naver.com>
2022-03-06 23:03:06 +0900

    init

--- src/main/java/com/example/springtodo/demo/model/UserEntity.java
+++ src/main/java/com/example/springtodo/demo/model/UserEntity.java
@@ -1,30 +1,38 @@
 package com.example.springtodo.demo.model;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
+@Data
 @Entity
-@Getter
-@Setter
-@ToString
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
 public class UserEntity {
-
     @Id
     @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name="system-uuid", strategy = "uuid")
-    private String id;
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id; // 유저에게 고유하게 부여되는 id.
 
     @Column(nullable = false)
-    private String username;
-
-    @Column(nullable = false, unique = true)
-    private String email;
+    private String username; // 유저의 이름
 
     @Column(nullable = false)
-    private String password;
+    private String email; // 유저의 email, 아이디와 같은 기능을 한다.
+
+    @Column(nullable = false)
+    private String password; // 패스워드. null이 가능한 이유는 oAuth로 페이스북이나 트위터같은 제3의 어플리케이션을 통해 로그인 할 수 있게 하기 위함이다.
+
 }
