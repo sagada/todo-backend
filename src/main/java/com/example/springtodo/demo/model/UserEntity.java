beaeb50804814efd9b2bb38f40780f9b963deccd
sagada <wlatmdqja@naver.com>
2022-03-08 23:48:42 +0900

    innit

--- src/main/java/com/example/springtodo/demo/model/UserEntity.java
+++ src/main/java/com/example/springtodo/demo/model/UserEntity.java
@@ -24,15 +24,15 @@ public class UserEntity {
     @Id
     @GeneratedValue(generator = "system-uuid")
     @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    private String id; // 유저에게 고유하게 부여되는 id.
+    private String id;
 
     @Column(nullable = false)
-    private String username; // 유저의 이름
+    private String username;
 
     @Column(nullable = false)
-    private String email; // 유저의 email, 아이디와 같은 기능을 한다.
+    private String email;
 
     @Column(nullable = false)
-    private String password; // 패스워드. null이 가능한 이유는 oAuth로 페이스북이나 트위터같은 제3의 어플리케이션을 통해 로그인 할 수 있게 하기 위함이다.
+    private String password;
 
 }
