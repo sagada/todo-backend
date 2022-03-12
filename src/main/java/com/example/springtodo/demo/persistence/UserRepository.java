454e2ff8937889a7c7ad13d755991a0db13599af
sagada <wlatmdqja@naver.com>
2022-03-12 17:36:07 +0900

    update; :
    :

--- src/main/java/com/example/springtodo/demo/persistence/UserRepository.java
+++ src/main/java/com/example/springtodo/demo/persistence/UserRepository.java
@@ -3,11 +3,12 @@ package com.example.springtodo.demo.persistence;
 import com.example.springtodo.demo.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     Boolean existsByEmail(String email);
     UserEntity findByEmailAndPassword(String email, String password);
-    UserEntity findByEmail(String email);
+    UserEntity findByEmail(@Param("email") String email);
 }
