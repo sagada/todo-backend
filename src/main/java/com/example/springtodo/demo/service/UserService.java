8ed77ffda2a26e84281f8028d69e3ef6f379c8c3
sagada <wlatmdqja@naver.com>
2022-03-12 17:31:00 +0900

    update

--- src/main/java/com/example/springtodo/demo/service/UserService.java
+++ src/main/java/com/example/springtodo/demo/service/UserService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
 
+    @Transactional
     public UserEntity create(final UserEntity userEntity)
     {
         if (userEntity == null || userEntity.getEmail() == null)
