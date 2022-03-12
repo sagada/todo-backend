9b46501b673971997432ee37c7d1f72fa6ff605e
sagada <wlatmdqja@naver.com>
2022-03-12 17:02:11 +0900

    repository annotation add

--- src/main/java/com/example/springtodo/demo/persistence/UserRepository.java
+++ src/main/java/com/example/springtodo/demo/persistence/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.springtodo.demo.persistence;
 
 import com.example.springtodo.demo.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     Boolean existsByEmail(String email);
     UserEntity findByEmailAndPassword(String email, String password);
